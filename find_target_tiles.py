a0018dd5d92c8cc8c3e622a25de521b199b0a6d2
Tobias Andermann <tobias.andermann@ebc.uu.se>
2024-02-21 16:28:10 +0100

    updated find_target_files to allow for polygon input

--- find_target_tiles.py
+++ find_target_tiles.py
@@ -1,48 +1,80 @@
 import os
 import re
 import argparse
+import geopandas as gpd
+from shapely.geometry import box
+import concurrent.futures
 import shutil
 
 
-def overlaps(window, tile):
-    return not (tile[2] < window[0] or tile[0] > window[2] or
-                tile[3] < window[1] or tile[1] > window[3])
+def overlaps(tile_bbox, polygon):
+    return tile_bbox.intersects(polygon)
 
 
-def find_overlapping_files(folder_path, cropping_window):
-    overlapping_files = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.tiff'):
+def check_file(filename, folder_path, geom):
+    if filename.endswith('.tiff'):
+        try:
             coords = list(map(int, re.findall(r'\d+', filename)[-4:]))  # x_min, y_min, x_max, y_max
-            if overlaps(cropping_window, coords):
-                overlapping_files.append(filename)
+            tile_bbox = box(coords[0], coords[1], coords[2], coords[3])
+            if overlaps(tile_bbox, geom):
+                return os.path.join(folder_path, filename)
+        except ValueError:
+            pass
+    return None
+
+
+def find_overlapping_files(folder_path, geom):
+    overlapping_files = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(check_file, filename, folder_path, geom)
+                   for filename in os.listdir(folder_path)]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                overlapping_files.append(result)
     return overlapping_files
 
 
-def copy_files(files, src_folder, dst_folder):
+def copy_files(files, dst_folder):
     os.makedirs(dst_folder, exist_ok=True)
     for file in files:
-        shutil.copy(os.path.join(src_folder, file), dst_folder)
+        shutil.copy(file, dst_folder)
 
 
 def main(opt):
-    overlapping_files = find_overlapping_files(opt.folder_path, (opt.x_min, opt.y_min, opt.x_max, opt.y_max))
+    # Load polygon geometry if a polygon file is provided
+    if opt.polygon_file:
+        gdf = gpd.read_file(opt.polygon_file)
+        polygon_geom = gdf.unary_union
+    else:
+        polygon_geom = box(opt.x_min, opt.y_min, opt.x_max, opt.y_max)
+
+    overlapping_files = find_overlapping_files(opt.folder_path, polygon_geom)
     print("Overlapping files:")
     for file in overlapping_files:
         print(file)
+
     # Copy the overlapping files to the output folder
     if overlapping_files:
-        copy_files(overlapping_files, opt.folder_path, opt.output_folder)
+        copy_files(overlapping_files, opt.output_folder)
         print(f"Copied {len(overlapping_files)} files to {opt.output_folder}")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Find and copy .tiff files overlapping with a specified window.')
+    parser = argparse.ArgumentParser(
+        description='Find and copy .tiff files overlapping with a specified window or polygon.')
     parser.add_argument('--folder_path', type=str, required=True, help='Path to the folder containing .tiff files')
     parser.add_argument('--output_folder', type=str, required=True, help='Path to the output folder for copying files')
-    parser.add_argument('--x_min', type=int, required=True, help='Minimum X coordinate of the cropping window')
-    parser.add_argument('--y_min', type=int, required=True, help='Minimum Y coordinate of the cropping window')
-    parser.add_argument('--x_max', type=int, required=True, help='Maximum X coordinate of the cropping window')
-    parser.add_argument('--y_max', type=int, required=True, help='Maximum Y coordinate of the cropping window')
+    parser.add_argument('--polygon_file', type=str, help='Path to the polygon file (.gpkg format)')
+    parser.add_argument('--x_min', type=int, help='Minimum X coordinate of the cropping window')
+    parser.add_argument('--y_min', type=int, help='Minimum Y coordinate of the cropping window')
+    parser.add_argument('--x_max', type=int, help='Maximum X coordinate of the cropping window')
+    parser.add_argument('--y_max', type=int, help='Maximum Y coordinate of the cropping window')
+
     opt = parser.parse_args()
+
+    # Check if either polygon file or coordinates are provided
+    if not opt.polygon_file and (opt.x_min is None or opt.y_min is None or opt.x_max is None or opt.y_max is None):
+        parser.error("Either --polygon_file or coordinates (--x_min, --y_min, --x_max, --y_max) must be provided.")
+
     main(opt)
