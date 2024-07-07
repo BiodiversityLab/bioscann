92e08c47598cc715a71eea1a9cfe01c707508649
Tobias Andermann <tobias.andermann@ebc.uu.se>
2024-07-07 17:45:12 +0200

    adapted pipeline to new environmental features

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # BIOSCANN
 BIOSCANN: BIOdiversity Segmentation and Classification with Artificial Neural Networks
 
+This tutorial shows the basic commands for running the `bioscann` pipeline to make predictions of conservation value across national scale (in this case applied to forests in Sweden). 
+This includes the following main steps:
+- installation of `bioscann` (installation tested on MacOS)
+- preparation of training data
+- model-training
+- predictions of conservation value
+
+## Download project from GitHub
+Download the `bioscann` GitHub repo and navigate into the project folder using your command line. For the installation in the next step you need to have the installation manager `conda` installed (download [here](https://docs.anaconda.com/miniconda/)).
+
 ## Installation
+You install the software as a conda package by executing the commands below in your command line. Before running the following installation commands, make sure you are located in the downloaded `bioscann` directory. For this to work the sub-directory `bin/AIRaster-dataprocessing` and the installation file `requirements.txt` need to be present in your directory. It is recommended to paste the commands below one by one to make sure the installation completes as expected.
 ```commandline
+conda create -y -n bioscann
+conda activate bioscann
 conda install -c conda-forge -y python=3.8
 conda install -c conda-forge -y fiona=1.9.3
+conda install -c conda-forge -y imagecodecs
 conda install -c conda-forge -y pytorch
 conda install -c conda-forge -y torchvision
 conda install -c conda-forge -y mlflow
@@ -14,59 +28,103 @@ cd ../..
 pip install -r requirements.txt
 ```
 
-## Define cropping window around each polygon
+## Convert polygon data into individual instances
+In this step we create the cropping windows for the individual instances that we use for model training and evaluation. For simplicity we show the `bioscann` wokflow for only one of the five separate bioregional subsets of the data, namely the southern boreal region.
 
-#### Boreal_east
 ```commandline
 python crop_windows_from_polygons.py \
-    --input_path data/polygons/boreal_east \
-    --output_path data/processed_geodata/boreal_east/cropped_windows \
+    --input_path data/polygons/boreal_south \
+    --output_path data/processed_geodata/boreal_south/cropped_windows \
+    --extent_size 1280 \
     --no_overlap
 ```
 
 
-## Extract spatial data for each cropping window
+## Download environmental features for each instance
+To download the remote-sensing derived environmental features for each instance (1.28 x 1.28 km cropping window), we need access to the data-server. For this it is required to create a user-account at https://www.skogsstyrelsen.se/sjalvservice/karttjanster/geodatatjanster/skaffa-anvandarkonto/ (application form in Swedish). For testing the data-download, it is possible to use a temporary access account with the following login-information:
+- username: `skstemp_user`
+- password: `S7Qawt3v`
 
-#### Boreal_east
+For this script to run, it requires the input folder `data/processed_geodata/boreal_south/cropped_windows` resulting from the previous command. You can either wait for the previous command to finish or download the compiled output of the previous script from the supplementary data uploaded with the `bioscann` manuscript. In the latter case, make sure it is placed in the right location in your working directory for the script to find it.
+
 ```commandline
-region="boreal_east"
 python extract_geo_data.py \
-    --output_path data/processed_geodata/${region}/${region}_geodata  \
-    --window_coordinates data/processed_geodata/${region}/cropped_windows \
-    --configuration version_public_sat \
+    --output_path data/processed_geodata/boreal_south/boreal_south_geodata  \
+    --window_coordinates data/processed_geodata/boreal_south/cropped_windows \
+    --configuration version_public_sat_2024 \
     --test_config version_1 \
     --testset_size 0.2 \
     --img-size 128 \
-    --username ***** \
-    --password ***** \
+    --username skstemp_user \
+    --password S7Qawt3v \
     --logging_off \
-    --threads 30
+    --threads 10
 ```
 
 ## Train model
-#### Boreal_east
-```commandline
-region="boreal_east"
-configuration="20,30,40,50,40,30,20"
+The next step is training the deep-learning model, which can be time-intensive depending on the size of the input data. For our southern boreal dataset this will likely take more than 1 day, but it can be spead up considerably when running on a machine that can utilize GPU resources (requires manual installation and GPU-mounting of the pytorch machine learning library, not covered in this tutorial). You can start training your model by running the command below, but we recommend to use one of our provided trained models (see supplementary data) for the following steps.
 
+```commandline
 python train_model.py \
-    --batch 14 \
+    --batch 5 \
     --input 11 \
     --output 1 \
     --algorithm AttentionPixelClassifierFlex \
-    --device gpu \
-    --dataset data/processed_geodata/${region}/${region}_geodata \
-    --validation data/processed_geodata/${region}/${region}_geodata/validation \
-    --test_dataset data/processed_geodata/${region}/${region}_geodata/testset/ \
+    --device cpu \
+    --dataset data/processed_geodata/boreal_south/boreal_south_geodata \
+    --validation data/processed_geodata/boreal_south/boreal_south_geodata/validation \
+    --test_dataset data/processed_geodata/boreal_south/boreal_south_geodata/testset/ \
     --plot \
-    --experiment_name ${region} \
+    --experiment_name trained_model_tutorial \
     --epochs 300 \
     --img_size 128 \
     --mlflow \
-    --n_channels_per_layer ${configuration} \
+    --n_channels_per_layer 100,50,100 \
     --pfi \
     --patience 20
 ```
+
+## Extract environmental features for predictions
+Now that we have a trained model, let us make predictions for an area of interest. The first step is to define the area and extract all needed environmental predictors for this area. You can define the area by providing the coordinates of the bottom left and the top right corner (using the [SWEREF 99](https://www.lantmateriet.se/en/geodata/gps-geodesi-och-swepos/reference-systems/three-dimensional-systems/SWEREF-99/) coordinate reference system). The script will draw a rectangle between the provided points, break up the area into 1.28 x 1.28 km tiles, and extract all environmental features for each tile.
+
+The trained model can only be applied to sites in Sweden, as several of the environmental features are only available within the country boundaries. To make sure all your tiles are within Sweden you can provide a cropping polygon of Sweden that will be used to filter your tiles. For this, use the `--sweden_map` flag and point to the provided shape-file: `'data/sweden_polygon/Sweref_99_TM/shape/swemap_gpkg/swemap.gpkg'`. (Note: To make predictions for all of Sweden, you can add the `--auto_adjust_prediction_range` flag, which will identify all tiles within the bounds of the Sweden polygon, independent of the provided input coordinates).
+
+In our implementation we download an additional buffer of 200 m around each tile to address the edge-effect (for explanation see manuscript). This is done by setting `--additional_offset -400`, adding a total of 400 m to the tile-size along the x and y-axis.
+
+```commandline
+python download_prediction_geodata.py \
+  --coordinates 522375,6416051,532938,6425134 \
+  --offset 1280 \
+  --image_size 128 \
+  --download_folder data/prediction_geodata/download_folder \
+  --configuration version_public_sat_2024 \
+  --meters_per_pixel 10 \
+  --image_scale 1 \
+  --additional_offset -400 \
+  --target_server https://geodata.skogsstyrelsen.se/arcgis/rest/ \
+  --username skstemp_user \
+  --password S7Qawt3v \
+  --threads 10 \
+  --sweden_map data/sweden_polygon/Sweref_99_TM/shape/swemap_gpkg/swemap.gpkg \
+  --auto_adjust_prediction_range
+```
+
+## Make predictions with trained model
+Now we use the trained model to make predictions for all tiles for which we downloaded the environmental features in the previous step. Since we applied a 200 m buffer to each tile, we need to remove this buffer from the predictions to convert each tile back to the target-tile-size. Therefore we apply the flag `--crop_corners 20`, which removes 20 pixels (at 10 m per pixel = 200 m) around each prediction image.
+
+```commandline
+python make_predictions.py \
+--geodata_folder data/prediction_geodata/download_folder \
+--output_path predictions/model_predictions_tutorial \
+--trained_model train/trained_model_tutorial/100,50,100/best_model.pth \
+--crop_corners 20 \
+--apply_mask \
+--configuration version_public_sat_2024 \
+--threads 10
+```
+
+
+
 
 ## Merge predictions into one spatial raster
 ```commandline
--- utils/RestAPIs.py
+++ utils/RestAPIs.py
@@ -212,6 +212,31 @@ def params_biomass(coordinates_list, image_year='', rendering_rule='', image_siz
     )
     return params
 
+def params_biomass_2024(coordinates_list, image_year='', rendering_rule='', image_size=512):
+    coordinates_string = str()
+    params = dict(
+        bbox='{},+{},+{},+{}'.format(coordinates_list[0], coordinates_list[1], coordinates_list[2],coordinates_list[3]),
+        bboxSR='3006',
+        size='{},{}'.format(image_size, image_size),
+        imageSR='',
+        time='',
+        format='tiff',
+        pixelType='UNKNOWN',
+        noData='',
+        noDataInterpretation='esriNoDataMatchAny',
+        interpolation='+RSP_BilinearInterpolation',
+        compression='LZ77',
+        compressionQuality='',
+        bandIds='4',
+        sliceId='',
+        adjustAspectRatio='true',
+        validateExtent='false',
+        lercVersion='1',
+        compressionTolerance='',
+        f='pjson',
+    )
+    return params
+
 def params_leaves_present(coordinates_list, image_year='', rendering_rule='', image_size=512):
     coordinates_string = str()
     params = dict(
@@ -229,6 +254,31 @@ def params_leaves_present(coordinates_list, image_year='', rendering_rule='', im
         compression='LZ77',
         compressionQuality='',
         bandIds='',
+        sliceId='',
+        adjustAspectRatio='true',
+        validateExtent='false',
+        lercVersion='1',
+        compressionTolerance='',
+        f='pjson',
+    )
+    return params
+
+def params_leaves_present_2024(coordinates_list, image_year='', rendering_rule='', image_size=512):
+    coordinates_string = str()
+    params = dict(
+        bbox='{},+{},+{},+{}'.format(coordinates_list[0], coordinates_list[1], coordinates_list[2],coordinates_list[3]),
+        bboxSR='3006',
+        size='{},{}'.format(image_size, image_size),
+        imageSR='',
+        time='',
+        format='tiff',
+        pixelType='UNKNOWN',
+        noData='',
+        noDataInterpretation='esriNoDataMatchAny',
+        interpolation='+RSP_BilinearInterpolation',
+        compression='LZ77',
+        compressionQuality='',
+        bandIds='8',
         sliceId='',
         adjustAspectRatio='true',
         validateExtent='false',
@@ -292,6 +342,64 @@ def params_bioclim(coordinates_list, image_year='', rendering_rule='', image_siz
     return params
 
 
+def params_maxtemp(coordinates_list, image_year='', rendering_rule='', image_size=512):
+    coordinates_string = str()
+    params = dict(
+        bbox='{},+{},+{},+{}'.format(coordinates_list[0], coordinates_list[1], coordinates_list[2],coordinates_list[3]),
+        bboxSR='3006',
+        size='{},{}'.format(image_size, image_size),
+        imageSR='',
+        time='1713225600000',
+        # time='1672527601000',
+        # MinDate='2023/01/01 00:00:00 UTC',
+        # MaxDate='2023/12/31 23:59:59 UTC',
+        # ImageYear='2023',
+        # NumDays='365',
+        format='tiff',
+        pixelType='UNKNOWN',
+        noData='',
+        noDataInterpretation='esriNoDataMatchAny',
+        interpolation='+RSP_BilinearInterpolation',
+        # renderingRule='None',
+        renderingRule='{"rasterfunction":"Maxtemp"}',
+        compression='LZ77',
+        compressionQuality='',
+        bandIds='1',
+        sliceId='',
+        adjustAspectRatio='true',
+        validateExtent='false',
+        lercVersion='1',
+        compressionTolerance='',
+        f='pjson',
+    )
+    return params
+
+def params_sumtemp(coordinates_list, image_year='', rendering_rule='', image_size=512):
+    coordinates_string = str()
+    params = dict(
+        bbox='{},+{},+{},+{}'.format(coordinates_list[0], coordinates_list[1], coordinates_list[2], coordinates_list[3]),
+        bboxSR='3006',
+        size='{},{}'.format(image_size, image_size),
+        imageSR='',
+        time='1713225600000',
+        format='tiff',
+        pixelType='UNKNOWN',
+        noData='',
+        noDataInterpretation='esriNoDataMatchAny',
+        interpolation='+RSP_BilinearInterpolation',
+        renderingRule='{"rasterfunction":"TempSumma"}',
+        compression='LZ77',
+        compressionQuality='',
+        bandIds='1',
+        sliceId='',
+        adjustAspectRatio='true',
+        validateExtent='false',
+        lercVersion='1',
+        compressionTolerance='',
+        f='pjson',
+    )
+    return params
+
 
 def url_land_cover_types():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Marktackedata_2_0/ImageServer/exportImage'
@@ -299,7 +407,11 @@ def url_land_cover_types():
 
 def url_treeheight():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Samverkan/Tradhojd/ImageServer/exportImage' #newer version
-#    url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Tradhojd/ImageServer/exportImage'
+    # url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Tradhojd/ImageServer/exportImage'
+    return url
+
+def url_treeheight_2024():
+    url = "https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Tradhojd_3_1/ImageServer/exportImage"
     return url
 
 def url_elevation():
@@ -314,12 +426,24 @@ def url_elevation_gradient():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Lutning/ImageServer/exportImage'
     return url
 
+def url_elevation_gradient_2024():
+    url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Lutning_1_0/ImageServer/exportImage'
+    return url
+
 def url_max_temp():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/CDS_Maxtemp_1_0/ImageServer/exportImage'
     return url
 
+def url_max_temp_2024():
+    url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/GBB_SvarmTemp_1_0/ImageServer/exportImage'
+    return url
+
 def url_sum_temp():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/CDS_Tempsum_1_0/ImageServer/exportImage'
+    return url
+
+def url_sum_temp_2024():
+    url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/GBB_SvarmTemp_1_0/ImageServer/exportImage'
     return url
 
 def url_ditches():
@@ -334,6 +458,10 @@ def url_basic_forest_attributes():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/SkogligaGrunddata/ImageServer/exportImage'
     return url
 
+def url_basic_forest_attributes_2024():
+    url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/SkogligaGrunddata_3_1/ImageServer/exportImage'
+    return url
+
 def url_sattelite_img():
     url = 'https://geodata.skogsstyrelsen.se/arcgis/rest/services/Publikt/Satellitdata_2_0/ImageServer/exportImage'
     return url
@@ -342,17 +470,23 @@ def url_sattelite_img():
 #Storing all functions in an object
 apis = {
     'land_cover_types': {'params': params_land_cover_types, 'url': url_land_cover_types,'normalization': PN.rescale_land_cover_types},
-    'treeheight': {'params': params_tree_height, 'url': url_treeheight,'normalization': PN.rescale_treeheight},
+    'treeheight': {'params': params_tree_height, 'url': url_treeheight, 'normalization': PN.rescale_treeheight},
+    'treeheight_2024': {'params': params_tree_height, 'url': url_treeheight_2024,'normalization': PN.rescale_treeheight},
     'elevation': {'params': params_elevation, 'url': url_elevation, 'normalization': PN.rescale_elevation}, # this is only available for part of the country
     "soil_moisture": {'params': params_soil_moisture, 'url': url_soil_moisture,'normalization': PN.rescale_soil_moisture},
     "elevation_gradient": {'params': standard_params, 'url': url_elevation_gradient,'normalization': PN.rescale_elevation_gradient},
+    "elevation_gradient_2024": {'params': standard_params, 'url': url_elevation_gradient_2024,'normalization': PN.rescale_elevation_gradient},
     "max_temp": {'params': standard_params, 'url': url_max_temp,'normalization': PN.rescale_max_temp},
+    "max_temp_2024": {'params': params_maxtemp, 'url': url_max_temp_2024, 'normalization': PN.rescale_max_temp},
     "sum_temp": {'params': standard_params, 'url': url_sum_temp,'normalization': PN.rescale_sum_temp},
+    "sum_temp_2024": {'params': params_sumtemp, 'url': url_sum_temp_2024,'normalization': PN.rescale_sum_temp},
     "ditches": {'params': standard_params, 'url': url_ditches,'normalization': PN.rescale_ditches},
     "peat_depth": {'params': params_peat_depth, 'url': url_soildata,'normalization': PN.rescale_peat_depth},
     "soil_type": {'params': params_soil_type, 'url': url_soildata,'normalization': PN.rescale_soil_type},
     "biomass": {'params': params_biomass, 'url': url_basic_forest_attributes,'normalization': PN.rescale_biomass},
+    "biomass_2024": {'params': params_biomass_2024, 'url': url_basic_forest_attributes_2024, 'normalization': PN.rescale_biomass},
     "leaves_present": {'params': params_leaves_present, 'url': url_basic_forest_attributes,'normalization': PN.rescale_leaves_present},
+    "leaves_present_2024": {'params': params_leaves_present_2024, 'url': url_basic_forest_attributes_2024,'normalization': PN.rescale_leaves_present},
     "satellite": {'params': params_satellite_img, 'url': url_sattelite_img,'normalization': PN.rescale_sattelite_img},
 }
     # "era5_bioclim":{'params': params_bioclim, 'url': url_bioclim,'normalization': PN.rescale_bioclim},
